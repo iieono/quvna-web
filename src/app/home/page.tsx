286101ee0b4248f89ec07cd848f0d87918ddc714
Abdusamadbek Akhamdjonov <axmadjonov.ti@gmail.com>
2025-01-02 21:50:14 +0500

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,24 @@
+"use client";
 import MainAdvertisements from "@/components/MainAdvertisements";
+import { useGetUserProfileQuery } from "@/features/authApi";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function pages() {
+  const [userId, setUserId] = useState<string | null>(null);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUserId = window.localStorage.getItem("userId");
+      setUserId(storedUserId);
+    }
+  }, []);
+  const { data, error, isLoading } = useGetUserProfileQuery(userId, {
+    skip: !userId,
+  });
   return (
     <div className="w-full h-full bg-primary-bg flex flex-col gap-2 lg:gap-5 overflow-y-auto pb-20 text-primary-text p-3 lg:p-10">
-      <div className="grid grid-cols-1 lg:grid-cols-2 justify-between gap-2 lg:gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-3 justify-between gap-2 lg:gap-5">
         <MainAdvertisements />
         <Link
           href="/shop"
@@ -20,6 +32,79 @@ function pages() {
           />
           shop
         </Link>
+        <div className="w-full h-full lg:h-96 p-3 lg:p-5 relative rounded-3xl lg:bg-white/10">
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between gap-1 h-16 items-center bg-white/10 rounded-lg text-primary-bg p-2 pl-4">
+              <div>
+                <p className="font-medium text-primary-text">
+                  {data?.data?.playName}
+                </p>
+                <p className="text-sm text-secondary-text">
+                  {data?.data?.gameID}
+                </p>
+              </div>
+              <div className="flex items-center">
+                <p className="font-medium text-reward-yellow">
+                  {data?.data?.rating?.ucAmount
+                    .toLocaleString("en", { useGrouping: true })
+                    .replace(/,/g, " ") || 0}
+                </p>
+                <Image
+                  src="/images/uc-icon.png"
+                  alt="UC icon"
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                  unoptimized
+                />
+              </div>
+            </div>
+            <div className="flex justify-between gap-1 h-16 items-center bg-white/10 rounded-lg text-primary-bg p-2 px-4">
+              <div>
+                <p className="font-medium text-primary-text">
+                  {data?.data?.steamName}
+                </p>
+              </div>
+              <div className="flex items-center gap-2">
+                <p className="font-medium text-reward-yellow">
+                  {data?.data?.rating?.steamAmount
+                    .toLocaleString("en", { useGrouping: true })
+                    .replace(/,/g, " ") || 0}
+                </p>
+                <Image
+                  src="/images/steam-icon.png"
+                  alt="Steam icon"
+                  width={24}
+                  height={24}
+                  className="rounded-full"
+                  unoptimized
+                />
+              </div>
+            </div>
+            <div className="flex justify-between gap-1 h-16 items-center bg-white/10 rounded-lg text-primary-bg p-2 px-4">
+              <div>
+                <p className="font-medium text-primary-text">
+                  {data?.data?.mobileLegendsName}
+                </p>
+              </div>
+              <div className="flex items-center gap-2">
+                <p className="font-medium text-reward-yellow">
+                  {data?.data?.rating?.mlamount
+                    .toLocaleString("en", { useGrouping: true })
+                    .replace(/,/g, " ") || 0}
+                </p>
+                <Image
+                  src="/images/ml-icon.png"
+                  alt="ML icon"
+                  width={24}
+                  height={24}
+                  className="rounded-full"
+                  unoptimized
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="grid grid-cols-2 lg:grid-cols-5 justify-between gap-2 lg:gap-5">
         <Link
--- src/components/MainAdvertisements.tsx
+++ src/components/MainAdvertisements.tsx
@@ -1,62 +1,110 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Image from "next/image"; // Importing the next/image component
+import Image from "next/image";
 import { useGetAdvertisementsQuery } from "@/features/extrasApi";
 import { useGetAttachmentQuery } from "@/features/ratingApi";
 
 function MainAdvertisements() {
   const { data, error, isLoading } = useGetAdvertisementsQuery("HOME_PAGE");
-  const [imageBlobUrls, setImageBlobUrls] = useState<string[]>([]);
   const [attachmentIds, setAttachmentIds] = useState<string[]>([]);
-  const [attachments, setAttachments] = useState<any[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     if (data?.data?.length > 0) {
-      // Extract all attachment IDs from advertisements data
       const ids = data.data.map(
         (advertisement: { attachmentId: string }) => advertisement?.attachmentId
       );
-      console.log(ids);
       setAttachmentIds(ids);
     }
   }, [data]);
-  //   useEffect(() => {
-  //     if (!attachmentIds) return;
-  //     for (let id in attachmentIds) {
-  //       const { data: attachmentData } = useGetAttachmentQuery(id);
-  //       setAttachments([...attachments, attachmentData]);
-  //       console.log(attachments);
-  //     }
-  //   }, [attachmentIds]);
+
+  useEffect(() => {
+    if (attachmentIds.length > 0) {
+      const intervalId = setInterval(() => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === attachmentIds.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 3000); // Change slide every 3 seconds
+
+      return () => clearInterval(intervalId); // Cleanup on component unmount
+    }
+  }, [attachmentIds]);
 
   return (
-    <div className="w-full h-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="w-full h-40 lg:h-96 relative rounded-3xl overflow-hidden">
       {isLoading ? (
         <p>Loading advertisements...</p>
       ) : error ? (
         <p>Error loading advertisements.</p>
       ) : (
-        imageBlobUrls.map((url, index) => (
-          <div
-            key={index}
-            className="w-full h-60 lg:h-96 rounded-3xl bg-white/10 flex items-center justify-center overflow-hidden"
-          >
-            <Image
-              src={url}
-              alt={`Advertisement ${index + 1}`}
-              className="w-full h-full object-cover"
-              width={800} // You can specify the desired width
-              height={400} // You can specify the desired height
-              onError={() => {
-                // Fallback to default image on error
-                setImageBlobUrls((prev) =>
-                  prev.map(() => "/images/default-ad.png")
-                );
-              }}
-            />
-          </div>
-        ))
+        attachmentIds?.length > 0 && (
+          <Advertisement id={attachmentIds[currentIndex]} />
+        )
       )}
+    </div>
+  );
+}
+
+function Advertisement({ id }: { id: any }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [imageBlobUrl, setImageBlobUrl] = useState<string>(
+    "/images/default-user.png"
+  );
+
+  const { data: attachmentImage, error, isLoading } = useGetAttachmentQuery(id);
+
+  useEffect(() => {
+    if (attachmentImage) {
+      const blobUrl = URL.createObjectURL(attachmentImage);
+      setImageBlobUrl(blobUrl);
+      setImageLoaded(true);
+
+      // Cleanup the URL when the component unmounts
+      return () => {
+        if (blobUrl.startsWith("blob:")) {
+          URL.revokeObjectURL(blobUrl);
+        }
+      };
+    } else {
+      // Fallback to the default image if there's no attachment
+      setImageBlobUrl("/images/default-user.png");
+      setImageLoaded(true);
+    }
+  }, [attachmentImage]);
+
+  const handleImageError = () => {
+    setImageBlobUrl("/images/default-user.png");
+    setImageLoaded(true);
+  };
+
+  if (isLoading) {
+    return (
+      <div className="animate-pulse hidden lg:flex">
+        <div className="w-[70px] h-[70px] bg-gray-200 rounded-tl-3xl rounded-br-3xl"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="text-red-500 hidden lg:flex">
+        Error loading profile data
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full h-60 lg:h-96 rounded-3xl bg-white/10 flex items-center justify-center overflow-hidden">
+      <Image
+        src={imageBlobUrl}
+        fill
+        alt="Advertisement image"
+        className={`object-cover transition-opacity duration-300 ${
+          imageLoaded ? "opacity-100" : "opacity-0"
+        }`}
+        onError={handleImageError}
+        unoptimized
+      />
     </div>
   );
 }
