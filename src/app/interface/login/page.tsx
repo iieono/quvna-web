35011b150c2ded7018ba1575145ebd1482e48aad
Abdusamadbek Akhamdjonov <axmadjonov.ti@gmail.com>
2025-01-15 13:55:44 +0500

    update

--- src/app/interface/login/page.tsx
+++ src/app/interface/login/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useLoginMutation } from "../../features/authApi";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { TextShimmer } from "@/components/ui/text-shimmer";
+import { useLoginMutation } from "@/features/authApi";
 
 function LoginPage() {
   const router = useRouter();
